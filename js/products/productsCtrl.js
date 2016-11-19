9f37546ea19eb512ea62af7534bdbe7adb884469
Matangi Kendapady <mtkendapady@gmail.com>
2016-11-19 11:08:12 -0600

    Completed project, expand on it further later

---
+++ js/products/productsCtrl.js
@@ -0,0 +1,9 @@
+angular.module( "miniRouting" )
+  .controller( "productsCtrl", function( $scope, $stateParams, productsService ) {
+    if($stateParams.id === "shoes" ) {
+      $scope.productData = productsService.shoeData;
+    }
+    else if($stateParams.id === "socks" ) {
+      $scope.productData = productsService.sockData;
+    }
+  });
